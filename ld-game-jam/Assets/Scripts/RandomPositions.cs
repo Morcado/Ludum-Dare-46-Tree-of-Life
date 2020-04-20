26e1237d2271306a1206335815513bd2416a70b9
Eduard <bakayaro.ed@gmail.com>
2020-04-21 01:12:27 +0300

    coordinates for random collectables and stage 3 created

---
+++ ld-game-jam/Assets/Scripts/RandomPositions.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPositions : MonoBehaviour
+{
+    double[] randomPositionsStageOne = {
+        -8.59, 1.96,
+        10.26, 2.03,
+        -9.67, 13.32,
+        -0.5, 13.06,
+        9.56, 13.17,
+        9.67, 9.11,
+        -0.24, 8.93,
+        -10, 9,
+        -14.43, 9.04,
+        -6.9, 5.09,
+        13.48, 6.13};
+
+    double[] randomPositionsStageTwo = {
+        -0.01, 11.93,
+        -9.23, 12.98,
+        -9.02, 12.95,
+        10.44, 8.01,
+        -9.1, 7.86,
+        -14.05, 9.85,
+        -12.84, 4.96,
+        -5.06, 5.08,
+        10.81, 4.9,
+        -13.58, 1.92,
+        11.81, 1.93};
+
+    double[] randomPositionsStageThree = {
+        -1.89, 14,
+        -6.93, 11.92,
+        6.99, 12.01,
+        -11.4, 8.96,
+        11.58, 8.96,
+        -0.04, 9.98,
+        -6.71, 5.91,
+        6.42, 6,
+        -12.05, 2.02,
+        12.05, 1.98,
+        2.22, 14};
+}
