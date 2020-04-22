1462d294807fa166ebf4cf4302ae87ba39c482a1
Eduard <bakayaro.ed@gmail.com>
2020-04-22 20:28:16 +0300

    enemy spawn pos

--- ld-game-jam/Assets/Scripts/EnemySpawnPositions.cs
+++ ld-game-jam/Assets/Scripts/EnemySpawnPositions.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class EnemySpawnPositions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    double[] enemySpawnPositionFirstStage = {
+        -10.13, 13.47,
+        14.33, 12,
+        14.08, 6.18,
+        -13.37, 8.47 };
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    double[] enemySpawnPositionSecondStage = {
+        12.89, 14.05,
+        -13.24, 14.13,
+        -14.05, 10.11,
+        -14.05, 5.4,
+        14.04, 2.16,
+        14.04, 8.47 };
+
+    double[] enemySpawnPositionThirdStage = {
+        -13.99, 14.37,
+        -14.02, 10.12,
+        -13.89, 6.07,
+        13.56, 14.07,
+        14.03, 10.26,
+        13.93, 6.04,
+        0, 13.87,
+        -0.71, 10.03};
 }
